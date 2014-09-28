c5428e292910c5c256664b8054dd25ffb687bb1a
edwin-navarrete <navarrete_edwin@yahoo.com.mx>
2014-09-28 17:54:56 -0500

    Pequeña mejora para obtener ref a TrackController

--- Assets/Scripts/CameraHorizontalMove.cs
+++ Assets/Scripts/CameraHorizontalMove.cs
@@ -2,16 +2,12 @@
 using System.Collections;
 
 public class CameraHorizontalMove : MonoBehaviour {
-
-	public GameObject trackControllerHolder;
+	
 	private TrackController2 trackController;
 
 	// Use this for initialization
 	void Start () {
-		if(trackControllerHolder == null){
-			trackControllerHolder = GameObject.Find("TrackControllerGo"); 
-			trackController = trackControllerHolder.GetComponent<TrackController2>();
-		}
+		trackController = GameObject.FindObjectOfType<TrackController2>(); 
 		print ("Camera Limit="+trackController.GetFinishLineX());
 		transform.position += Vector3.right * (transform.position.x - trackController.GetStartLineX());
 	}
