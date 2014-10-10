587c7fee21fc543525671ee325583ae41ded80b3
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2014-10-09 19:52:02 -0500

    Cambio de carril con jugador
    Si el jugador intenta cambiar de carril y en éste carril se encuentra un
    jugador más adelante, el jugador que cambia de carril de se ve
    penalizado en velocidad. Este mismo efecto sucede en el caso contrario

--- Assets/Scripts/BackScripts/RunnerController.cs
+++ Assets/Scripts/BackScripts/RunnerController.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (Animator), typeof (Renderer))]
+[RequireComponent (typeof (Animator), typeof (Renderer), typeof (BoxCollider))]
 public class RunnerController : MonoBehaviour
 {
 	public enum CharacterState
@@ -13,6 +13,7 @@ public class RunnerController : MonoBehaviour
 		Changing_Track
 	}
 
+	#region Variables
 	public float speed = 1.0f;
 	public float changeTrackSmooth = 1.0f;
 	public float nextPosDistance = 0.2F;
@@ -27,17 +28,26 @@ public class RunnerController : MonoBehaviour
 	[SerializeField]
 	private CharacterState curState;
 	private CharacterState nextState;
+
 	private TrackController tracker;
+
 	private float nextX;
 	private float currZ;
 	private float nextZ;
-	private Animator anim;
 	private float nextTime;
+	
+	private Animator anim;
+
+	private Collider _collider;
+	private Rigidbody _rigidbody;
+	#endregion
 
 	private void Awake ()
 	{
 		anim = GetComponent<Animator> ();
 		tracker = TrackController.instance;
+		_collider = GetComponent<BoxCollider> ();
+		_rigidbody = rigidbody;
 	}
 
 	private void Start ()
@@ -47,7 +57,7 @@ public class RunnerController : MonoBehaviour
 		nextTime = Time.time + timeDelay;
 	}
 
-	#region Enable/Disable
+	#region Enable and Disable callbacks
 
 	private void OnEnable ()
 	{
@@ -65,14 +75,22 @@ public class RunnerController : MonoBehaviour
 
 	public void TrackUp ()
 	{
-		nextZ = tracker.GetLaneAbove (currZ);
-		nextState = CharacterState.Changing_Track;
+		var newZ = tracker.GetLaneAbove (currZ);
+		if (CheckNextTrack (newZ, transform.forward))
+		{
+			nextZ = newZ;
+			nextState = CharacterState.Changing_Track;
+		}
 	}
 
 	public void TrackDown ()
 	{
-		nextZ = tracker.GetLaneBelow (currZ);
-		nextState = CharacterState.Changing_Track;
+		var newZ = tracker.GetLaneBelow (currZ);
+		if (CheckNextTrack (newZ, -transform.forward))
+		{
+			nextZ = newZ;
+			nextState = CharacterState.Changing_Track;
+		}
 	}
 
 	public void Jump ()
@@ -85,7 +103,7 @@ public class RunnerController : MonoBehaviour
 	private void StartRunning ()
 	{
 		nextState = CharacterState.Running;
-		nextX = transform.position.x + speed * speedFactor * (timeDelay);;
+		nextX = _rigidbody.position.x + speed * speedFactor * (timeDelay);;
 		anim.SetTrigger ("IsRunning");
 	}
 
@@ -101,9 +119,11 @@ public class RunnerController : MonoBehaviour
 		}
 
 		// En todo momento se estara realizando una aproximacion al lugar al cual el jugador debe de estar cada 200 millis
-		transform.position = Vector3.Lerp (transform.position, new Vector3 (nextX, transform.position.y, currZ), Time.deltaTime);
+		_rigidbody.position = Vector3.Lerp (_rigidbody.position, new Vector3 (nextX, _rigidbody.position.y, currZ), Time.deltaTime);
 		anim.SetFloat ("Speed", speedFactor);
 	}
+
+	#region States implementation
 
 	private void ChangeState ()
 	{
@@ -154,7 +174,7 @@ public class RunnerController : MonoBehaviour
 
 		case CharacterState.Running:
 			nextTime = Time.time + timeDelay;
-			nextX = transform.position.x + speed * speedFactor * (timeDelay);
+			nextX = _rigidbody.position.x + speed * speedFactor * (timeDelay);
 			break;
 
 		case CharacterState.Jumping:
@@ -165,21 +185,31 @@ public class RunnerController : MonoBehaviour
 
 		case CharacterState.Changing_Track:
 			nextTime = Time.time + timeDelay;
-			nextX = transform.position.x + speed * speedFactor * (timeDelay);
+			nextX = _rigidbody.position.x + speed * speedFactor * (timeDelay);
 			currZ = nextZ;
 			nextState = CharacterState.Running;
 			break;
 		}
 	}
 
+	#endregion
+
+	#region Obstacle Effects
+	 /*
+	  * Esta funcion hara caer al personaje poniendolo en un estado de espera paara que
+	  * el jugador lo levante
+	  * */
 	public void Fall ()
 	{
-		nextX = transform.position.x + speed * speedFactor * (timeDelay) + fallingDistance;
+		nextX = _rigidbody.position.x + speed * speedFactor * (timeDelay) + fallingDistance;
 		anim.SetBool ("Fall", true);
 		StopCoroutine ("RaiseSpeed");
 		nextState = CharacterState.Falling;
 	}
 
+	/*
+	 * Funcion que levanta al personaje una vez el jugador ha hecho click/touch encima del personaje
+	 * */
 	public void GetUp ()
 	{
 		// Levantara al jugador cuando se le de click en el
@@ -191,6 +221,17 @@ public class RunnerController : MonoBehaviour
 		}
 	}
 
+	/*
+	 * Esta funcion ralentizara al personaje dependiendo del valor que se le pase como parametro
+	 * @params float
+	 * */
+	public void SlowDown (float newSpeedFactor)
+	{
+		StopCoroutine ("RaiseSpeed");
+		speedFactor = newSpeedFactor;
+		StartCoroutine ("RaiseSpeed");
+	}
+
 	private IEnumerator RaiseSpeed ()
 	{
 		while (speedFactor < 1.0f)
@@ -199,4 +240,45 @@ public class RunnerController : MonoBehaviour
 			yield return null;
 		}
 	}
+
+	#endregion
+
+	#region Change track implementation
+	/*
+	 * La funcion recibe la nueva posicion en Z y un vector de direccion para saber si en el carril
+	 * al que pertenece el nuevo Z se encuentra un personaje. Si hay un personaje y menos del 50% de el
+	 * se encuetra delante o detras de este jugador, retorna true, sino retorna false
+	 * @params float, Vector3
+	 * @return bool
+	 * */
+	private bool CheckNextTrack (float newZ, Vector3 direction)
+	{
+		var distance = Mathf.Abs (newZ - _rigidbody.position.z);
+		RaycastHit hit;
+
+		// El rayo saldra del centro del sprite para saber
+		if (Physics.Raycast (_collider.bounds.center, direction, out hit, distance))
+		{
+			// Si el rayo golpea a un jugador no podra pasar al siguiente carril
+			return false;
+		}
+		// Si el rayo no golpea a un jugador, se puede pasar al siguiente carril
+		return true;
+	}
+
+	/*
+	 * Al momento de pasar a otro carril el jugador que se encuentre delante puede tocar el
+	 * collider del jugador que se encuentre atras para ralentizarlo
+	 * */
+	private void OnTriggerEnter (Collider other)
+	{
+		if (other.transform.tag == "Player")
+		{
+			if ((other.rigidbody.position - _rigidbody.position).x > 0)
+			{
+				SlowDown (0.5f);
+			}
+		}
+	}
+	#endregion
 }
