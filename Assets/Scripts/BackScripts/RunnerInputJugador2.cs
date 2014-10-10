5aa0f5f9beaa113342a03bfdf7e33c9d35793e90
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2014-10-09 21:40:54 -0500

    Input jugador 2

--- Assets/Scripts/BackScripts/RunnerInputJugador2.cs
+++ Assets/Scripts/BackScripts/RunnerInputJugador2.cs
@@ -2,43 +2,43 @@
 using System.Collections;
 
 [RequireComponent (typeof (RunnerController))]
-public class RunnerInput : MonoBehaviour
+public class RunnerInputJugador2 : MonoBehaviour
 {
 	private TrackController tracker;
 	private RunnerController controller;
-
+	
 	private void Awake ()
 	{
 		controller = GetComponent<RunnerController> ();
 	}
-
+	
 	private void FixedUpdate ()
 	{
-
-		if (Input.GetButtonDown ("Vertical"))
+		
+		if (Input.GetButtonDown ("Vertical2"))
 		{
-			if (Input.GetAxisRaw ("Vertical") == 1f)
+			if (Input.GetAxisRaw ("Vertical2") == 1f)
 			{
 				controller.TrackUp ();
 			}
 			
-			if (Input.GetAxisRaw ("Vertical") == -1f)
+			if (Input.GetAxisRaw ("Vertical2") == -1f)
 			{
 				controller.TrackDown ();
 			}
 		}
 		
-		if (Input.GetButtonDown ("Jump"))
+		if (Input.GetButtonDown ("Jump2"))
 		{
 			controller.Jump ();
 		}
 		
-		if (Input.GetButtonDown ("Ability"))
+		if (Input.GetButtonDown ("Ability2"))
 		{
 			//controller.Ability ();
 		}
 	}
-
+	
 	private void OnMouseUpAsButton ()
 	{	
 		// El metodo GetUp solo reacionara cuando el personaje se encuentre en el estado Falling
