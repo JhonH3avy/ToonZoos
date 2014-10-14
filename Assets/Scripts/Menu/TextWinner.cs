4eeb6ad0e479037bd671d7cd0abc17746cbe9ab5
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2014-10-13 21:11:40 -0500

    Texto de Evento ganador

---
+++ Assets/Scripts/Menu/TextWinner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (UILabel))]
+public class TextWinner : MonoBehaviour
+{
+	[SerializeField] private UILabel _label;
+	[SerializeField] private UISprite _sprite;
+
+	private void OnEnable ()
+	{
+		TrackController.FinishingRace += ShowWinner;
+	}
+
+	private void OnDisable ()
+	{
+		TrackController.FinishingRace -= ShowWinner;
+	}
+
+	private void Start ()
+	{		
+		_sprite.enabled = false;
+	}
+
+	private void ShowWinner (FinishingRaceArgs args)
+	{
+		_sprite.enabled = true;
+		_label.text = "The Winner is " + args.Winner.name;
+	}
+}
